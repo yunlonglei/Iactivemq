33a7a29b977a55a87a779a003aab32697d5a6dcf
leiyunlong <1175333149@qq.com>
2019-07-12 15:05:09 +0800

    topic消息修改

--- src/main/java/com/lei/active/topic/JmsConsumerMessageListener.java
+++ src/main/java/com/lei/active/topic/JmsConsumerMessageListener.java
@@ -22,10 +22,10 @@ public class JmsConsumerMessageListener {
             //3.创建会话session 第一个参数叫事务，第二个参数叫签收
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Topic topic = session.createTopic(TOPIC_NAME);
-            /**
+            /*
              * 异步非阻塞式方式：监听器 onMessage();
-             *              订阅者或者接收者通过MessageConsumer的setMessageListener(MessageListener  listener)注册一个消息监听器，
-             *              当消息到达后，系统会自动调用MessageListener的onMassage(Massges massage)方法；
+             * 订阅者或者接收者通过MessageConsumer的setMessageListener(MessageListener  listener)注册一个消息监听器，
+             * 当消息到达后，系统会自动调用MessageListener的onMassage(Massges massage)方法；
              */
             MessageConsumer consumer = session.createConsumer(topic);
             //创建消费者之后，用消费者来监听消息队列
